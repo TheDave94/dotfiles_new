f3bc946493dffb0d2de9a4bc8d0aa8c1d1fd2846
f0xb17 <102655383+f0xb17@users.noreply.github.com>
2025-01-17 21:34:08 +0100

    Changed File Permissions and added Gaming Script

---
+++ gaming.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+echo "--- Performing System Updates ---"
+
+sudo pacman -Syyuu --noconfirm
+
+echo "--- Installing Software ---"
+
+if lspci | grep -i "nvidia" > /dev/null; then
+    sudo pacman -S --needed --noconfirm lib32-nvidia-utils
+fi
+
+pkgs=(
+  "steam"
+  "wine"
+  "lutris"
+  "lib32-libxcomposite"
+  "cabextract"
+  "wine-mono"
+)
+
+for pkg in ${pkgs[@]}; do
+  echo "-- Installing: $pkg"
+  sudo pacman -S --needed --noconfirm ${pkg}
+done
+
+echo "--- Installing AUR Software ---"
+
+aurpkgs=(
+  "heroic-games-launcher-bin"
+  "protonup-qt"
+)
+
+for pkg in ${aurpkgs[@]}; do
+  echo "-- Installing: $pkg"
+  yay -S --needed --noconfirm ${pkg}
+done
+
+echo "--- Cleaning Up ---"
+
+yay -Scc --noconfirm
+yay -Yc --noconfirm
+
+sudo mkinitcpio -P
+
+echo "--- Rebooting ---"
+
+echo "-- Installation Finished"
+echo "-- Press enter to continue --"
+read
+
+reboot
